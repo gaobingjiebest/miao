6676bbf4ad85df5e2d9d3c276568d313b0d1843c
gbl <240068169@qq.com>
2018-07-16 11:44:44 +0800

    the one 5.0.3

--- lodash/gaobingjiebest-lodash.js
+++ lodash/gaobingjiebest-lodash.js
@@ -1,5 +1,20 @@
 var gaobingjiebest = {
   chunk: function(array, size) {
-    
+    let len = array.length
+    let res = []
+    let item = []
+    let n = 0
+
+    for (var i = 0; i < len; i++) {
+      item[n] = array[i]
+      n++
+      if (n === size) {
+        res.push(item)
+        item = []
+        n = 0
+      }
+    }
+
+    return res
   }
 }
